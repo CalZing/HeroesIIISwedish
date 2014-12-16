c88d7563c9149b6efe196bcc1002fbbaaabe9ff3
CalZing <andreas@irestal.se>
2014-12-16 20:09:59 +0100

    Lägg till korrekturläsnings-skript

---
+++ verktyg/skript/korrektur.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# Cleaning script for H3 Swedish. Adds/Removes necessary linebreaks
+# and restores Unicode damage from certain editors
+
+sedcmd=sed
+# use gsed if available (OS X)
+which gsed > /dev/null && sedcmd=gsed
+
+svdir=../../text/
+[ $1 ] && svdir=$1
+endir=../../text/original/
+[ $2 ] && endir=$2
+
+for f in $svdir*.txt $svdir*.TXT
+do
+  fname=$($sedcmd -e 's&.*\/&&g' <<< $f)
+  echo "$fname? (y/n/b)"
+  read check
+  [ $check != "y" ] || {
+    vim -O2 $endir$fname $svdir$fname
+  }
+  [ $check != "b" ] || {
+    break
+  }
+
+  # Create file containing EOL information
+done
+
+
+
+
+
