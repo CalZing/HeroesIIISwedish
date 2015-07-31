ba843383c6c15016ecd58ddeedb1bca43c6f393c
CalZing <andreas@irestal.se>
2015-08-01 01:40:33 +0200

    Återupptagit arbetet. Korrekturläsning coh småfixar

--- verktyg/skript/build_rel.sh
+++ verktyg/skript/build_rel.sh
@@ -13,17 +13,16 @@ endir=../../text/original/
 [ $2 ] && endir=$2
 reldir=../../release/Data/
 [ $3 ] && reldir=$3
+hdreldir=../../release/_HD3_Data/Lang/
+[ $3 ] && hdreldir=$4
 
-for f in $svdir*.txt $svdir*.TXT
+hdfile=\#sv.ini
+for f in $svdir*.txt $svdir*.TXT $svdir*.ini
 do
   fname=$($sedcmd -e 's&.*\/&&g' <<< $f)
   echo $fname
 
-  # Create file containing EOL information
   ./txtfix -i $svdir$fname -o $reldir$fname -l $endir$fname -t latin1 -b
 done
 
-
-
-
-
+mv "$reldir"en.ini "$hdreldir"\#sv.ini
